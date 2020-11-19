551aae65cfe7caa2d23f2a6cbc348d332954d906
davien21 <chidiebereekennia@gmail.com>
2020-11-19 11:20:02 +0100

    remove unneeded body object

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,8 +3,7 @@ let login_btn = document.querySelector('form button')
 
 
 login_btn.onclick = () => {
-  let body = {}
-  body.email = login_form.querySelector("input[name=email]").value
-  body.pass = login_form.querySelector("input[name=pass]").value
-  console.log(body)
+  let email = login_form.querySelector("input[name=email]").value
+  let pass = login_form.querySelector("input[name=pass]").value
+  console.log(email, pass)
 }
